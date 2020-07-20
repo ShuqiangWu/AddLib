9cc7ce405dec7ccfe7fc6fc384ae476d2e2a6bdb
13817519852@163.com <13817519852@163.com>
2020-07-20 16:26:38 +0800

    second commit

---
+++ cmelibrary/src/main/java/com/cmelibrary/HeatChecking.kt
@@ -0,0 +1,21 @@
+package com.cmelibrary
+
+import android.content.Context
+import android.widget.Toast
+
+/**
+ *@ClassName HeatChecking
+ *@Description 体温监测类
+ *@Author woodie
+ *@Date 7/19/20 6:31 PM
+ */
+
+class HeatChecking {
+
+    companion object {
+
+        fun getYaHa(): String? {
+            return "YaHa"
+        }
+    }
+}
